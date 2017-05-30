8d99d9c348e81f50de5059b41c7e5312f0647c24
Masayuki Morita <minamijoyo@gmail.com>
2017-05-30 23:32:12 +0900

    Report positions of the detected problems

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"go/ast"
 	"go/parser"
 	"go/token"
+	"io/ioutil"
 	"os"
 
 	// "github.com/k0kubun/pp"
@@ -23,6 +24,18 @@ func main() {
 	}
 }
 
+type file struct {
+	astFile  *ast.File
+	fset     *token.FileSet
+	src      []byte
+	filename string
+}
+
+type problem struct {
+	position token.Position
+	text     string
+}
+
 func lint(filename string) error {
 	fset := token.NewFileSet()
 	astFile, err := parser.ParseFile(fset, filename, nil, 0)
@@ -30,8 +43,56 @@ func lint(filename string) error {
 		return errors.Wrapf(err, "failed to parse file: %s", filename)
 	}
 	// pp.Println(astFile)
-	ast.Walk(walker(visitor), astFile)
+
+	src, err := ioutil.ReadFile(filename)
+	if err != nil {
+		return errors.Wrapf(err, "failed to read file: %s", filename)
+	}
+
+	f := &file{
+		astFile:  astFile,
+		fset:     fset,
+		src:      src,
+		filename: filename,
+	}
+
+	f.lint()
 	return nil
+}
+
+func (f *file) lint() {
+
+	check := func(ident *ast.Ident) {
+		name := ident.Name
+		if keywords[name] {
+			text := fmt.Sprintf("%s was found\n", name)
+			p := f.errorf(ident, text)
+			fmt.Printf("%v: %s", p.position, p.text)
+		}
+	}
+
+	visitor := func(node ast.Node) bool {
+		switch v := node.(type) {
+		case *ast.AssignStmt:
+			for _, expr := range v.Lhs {
+				if ident, ok := expr.(*ast.Ident); ok {
+					check(ident)
+				}
+			}
+		}
+		return true
+	}
+
+	ast.Walk(walker(visitor), f.astFile)
+}
+
+func (f *file) errorf(n ast.Node, text string) *problem {
+	pos := f.fset.Position(n.Pos())
+	problem := &problem{
+		position: pos,
+		text:     text,
+	}
+	return problem
 }
 
 type walker func(ast.Node) bool
@@ -43,25 +104,6 @@ func (w walker) Visit(node ast.Node) ast.Visitor {
 	return nil
 }
 
-func visitor(node ast.Node) bool {
-	switch v := node.(type) {
-	case *ast.AssignStmt:
-		for _, expr := range v.Lhs {
-			if ident, ok := expr.(*ast.Ident); ok {
-				check(ident)
-			}
-		}
-	}
-	return true
-}
-
 var keywords = map[string]bool{
 	"wally": true,
-}
-
-func check(ident *ast.Ident) {
-	name := ident.Name
-	if keywords[name] {
-		fmt.Println(name)
-	}
 }
