c771e5bd07f217db0405664a8f3a5a9c4a411909
Masayuki Morita <minamijoyo@gmail.com>
2017-05-31 22:50:51 +0900

    Generate random name data

---
+++ data/gen.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/Pallinder/go-randomdata"
+)
+
+func main() {
+	for i := 0; i < 10; i++ {
+		for j := 0; j < 5; j++ {
+			str := fmt.Sprintf("%s := %d; ", randomdata.SillyName(), i*10+j)
+			fmt.Print(str)
+		}
+		fmt.Println("")
+	}
+}
---
+++ data/wally2.go
@@ -0,0 +1,14 @@
+package main
+
+func main() {
+	Chestcrystal := 0; Griffincurse := 1; Falconwest := 2; Knightbattle := 3; Soarerjasper := 4;
+	Fishersand := 10; Dutchesssilent := 11; Kittenspring := 12; Lasherpink := 13; Legsvivid := 14;
+	Guardianabalone := 20; Noserapid := 21; Pigrust := 22; Warlockcarnelian := 23; Ringernorth := 24;
+	Crushergold := 30; Lanternchestnut := 31; Raptornarrow := 32; Swallowcold := 33; Songbald := 34;
+	Burnfuschia := 40; Spiritbald := 41; Toeprickle := 42; Graspdenim := 43; Bisonember := 44;
+	Fingerflannel := 50; Skullscythe := 51; Bugwax := 52; Skinnerrelic := 53; Biterjelly := 54;
+	Foegrove := 60; wally := 61; Serpentmirage := 62; Shakervoid := 63; Devourerbird := 64;
+	Houndpaint := 70; Finnarrow := 71; Playergray := 72; Lighterpollen := 73; Pigevening := 74;
+	Spiderbrave := 80; Mindbird := 81; Hawkspring := 82; Frightsticky := 83; Banedaffodil := 84;
+	Zebragossamer := 90; Tradergreen := 91; Herolinen := 92; Browred := 93; Faceprism := 94;
+}
