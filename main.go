27ae04309137f107fbe93686945766eec83ff71e
Masayuki Morita <minamijoyo@gmail.com>
2017-06-03 16:41:40 +0900

    Simplify for explanation

--- main.go
+++ main.go
@@ -5,18 +5,12 @@ import (
 	"go/ast"
 	"go/parser"
 	"go/token"
-	"io/ioutil"
 	"os"
 
-	// "github.com/k0kubun/pp"
 	"github.com/pkg/errors"
 )
 
 func main() {
-	if len(os.Args) != 2 {
-		fmt.Fprintln(os.Stderr, "Usage: minilint filename")
-		os.Exit(1)
-	}
 	filename := os.Args[1]
 	if err := lint(filename); err != nil {
 		fmt.Fprintln(os.Stderr, err)
@@ -27,13 +21,7 @@ func main() {
 type file struct {
 	astFile  *ast.File
 	fset     *token.FileSet
-	src      []byte
 	filename string
-}
-
-type problem struct {
-	position token.Position
-	text     string
 }
 
 func lint(filename string) error {
@@ -42,17 +30,10 @@ func lint(filename string) error {
 	if err != nil {
 		return errors.Wrapf(err, "failed to parse file: %s", filename)
 	}
-	// pp.Println(astFile)
-
-	src, err := ioutil.ReadFile(filename)
-	if err != nil {
-		return errors.Wrapf(err, "failed to read file: %s", filename)
-	}
 
 	f := &file{
 		astFile:  astFile,
 		fset:     fset,
-		src:      src,
 		filename: filename,
 	}
 
@@ -61,22 +42,12 @@ func lint(filename string) error {
 }
 
 func (f *file) lint() {
-
-	check := func(ident *ast.Ident) {
-		name := ident.Name
-		if keywords[name] {
-			text := fmt.Sprintf("%s was found\n", name)
-			p := f.errorf(ident, text)
-			fmt.Printf("%v: %s", p.position, p.text)
-		}
-	}
-
 	visitor := func(node ast.Node) bool {
 		switch v := node.(type) {
 		case *ast.AssignStmt:
 			for _, expr := range v.Lhs {
 				if ident, ok := expr.(*ast.Ident); ok {
-					check(ident)
+					f.check(ident)
 				}
 			}
 		}
@@ -86,15 +57,6 @@ func (f *file) lint() {
 	ast.Walk(walker(visitor), f.astFile)
 }
 
-func (f *file) errorf(n ast.Node, text string) *problem {
-	pos := f.fset.Position(n.Pos())
-	problem := &problem{
-		position: pos,
-		text:     text,
-	}
-	return problem
-}
-
 type walker func(ast.Node) bool
 
 func (w walker) Visit(node ast.Node) ast.Visitor {
@@ -102,6 +64,15 @@ func (w walker) Visit(node ast.Node) ast.Visitor {
 		return w
 	}
 	return nil
+}
+
+func (f *file) check(ident *ast.Ident) {
+	name := ident.Name
+	if keywords[name] {
+		pos := f.fset.Position(ident.Pos())
+		text := fmt.Sprintf("%s was found\n", name)
+		fmt.Printf("%v: %s", pos, text)
+	}
 }
 
 var keywords = map[string]bool{
